703206d628770fad6ca9c7f8b2126f70e6f50bf2
DefinitelyNotASpruce <justjolantux@gmail.com>
2022-05-26 22:20:03 +0300

    .

--- lib/views/call_info.dart
+++ lib/views/call_info.dart
@@ -13,35 +13,44 @@ class CallInfo extends StatefulWidget {
 class _SingleComment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
+    return Column(
       children: const <Widget>[
-        ListTile(
-          leading: Icon(
-            Icons.account_circle_outlined,
-            size: 35,
-          ), //Icon goes here
-          title: Text(
-            "Petras",
-            //style: TextStyle(fontSize: 25, height: 2),
-          ),
-          subtitle: Text("Skambina pastoviai sitas numeris ",
-              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic)),
-        )
+    ListTile(
+      leading: Icon(
+        Icons.account_circle_outlined,
+        size: 35,
+      ), //Icon goes here
+      title: Text(
+        "Petras",
+        //style: TextStyle(fontSize: 25, height: 2),
+      ),
+      subtitle: Text("Skambina pastoviai sitas numeris ",
+          style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic)),
+    )
       ],
-    ));
+    );
   }
 }
 
 class _CallInfoState extends State<CallInfo> {
   @override
   Widget build(BuildContext context) {
+    // Full screen width and height
+
+    double height = MediaQuery.of(context).size.height;
+
+// Height (without SafeArea)
+    var padding = MediaQuery.of(context).viewPadding;
+
+// Height (without status and toolbar)
+    double height3 = height - padding.top - kToolbarHeight;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Who Called Me?"),
       ),
-      body: Stack(children: <Widget>[
-        Center(
+      body: Column(children: <Widget>[
+        Expanded(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -84,7 +93,7 @@ class _CallInfoState extends State<CallInfo> {
                 ),
               ),
               SizedBox(
-                height: 600,
+                height: height3 - 280,
                 child: ListView.separated(
                   physics: const ClampingScrollPhysics(),
                   shrinkWrap: true,
@@ -101,24 +110,18 @@ class _CallInfoState extends State<CallInfo> {
                         style: TextStyle(
                             fontSize: 16, fontStyle: FontStyle.italic)),
                   ),
-                  itemCount: 2,
+                  itemCount: 100,
                 ),
               )
             ],
           ),
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
-
-          ///Your TextBox Container
-          child: Container(
-              height: 20.0,
-              padding: const EdgeInsets.symmetric(vertical: 2.0),
-              alignment: Alignment.bottomCenter,
-              child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                // First child is enter comment text input
-                TextFormField(
-                  //controller: commentController,
+        Container(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              // First child is enter comment text input
+              Expanded(
+                child: TextFormField(
                   autocorrect: false,
                   decoration: const InputDecoration(
                     labelText: "Some Text",
@@ -130,14 +133,14 @@ class _CallInfoState extends State<CallInfo> {
                         borderSide: BorderSide(color: Colors.purpleAccent)),
                   ),
                 ),
-                // Second child is button
-                const IconButton(
-                  icon: Icon(Icons.send),
-                  iconSize: 20.0,
-                  onPressed: null,
-                )
-              ])),
-        )
+              ),
+              // Second child is button
+              IconButton(
+                icon: const Icon(Icons.send),
+                iconSize: 40.0,
+                onPressed: () {},
+              )
+            ])),
       ]),
     );
   }
